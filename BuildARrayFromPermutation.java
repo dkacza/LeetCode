fb19bcba87dfb54aeb9c6a9b9020eede1b3a9dbf
Dawid Kacza <dawidkacza02@gmail.com>
2023-06-30 20:34:25 +0200

    LeetCode 30.06.2023

---
+++ BuildARrayFromPermutation.java
@@ -0,0 +1,8 @@
+public class BuildARrayFromPermutation {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) result[i] = nums[nums[i]];
+        return result;
+    }
+}
---
+++ ConcatenationOfArray.java
@@ -0,0 +1,11 @@
+public class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n * 2];
+        for(int i = 0; i < n; i++) {
+            result[i] = nums[i];
+            result[i + n] = nums[i];
+        }
+        return result;
+    }
+}
